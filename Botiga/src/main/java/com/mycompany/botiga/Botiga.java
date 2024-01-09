64ee26dbb412dd85c79e732f747cd307b4006b80
Lleyton27 <lleyton.camram.dam@insalfonscostafreda.cat>
2024-01-09 18:34:31 +0100

    Botiga

---
+++ Botiga/src/main/java/com/mycompany/botiga/Botiga.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.botiga;
+
+/**
+ *
+ * @author lleyton.camram.dam_i
+ */
+public class Botiga {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
