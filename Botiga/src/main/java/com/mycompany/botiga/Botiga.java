9d8eeecf3f73f71d1183ea729de2ac75431ead4b
anasscosta <152314133+anasscosta@users.noreply.github.com>
2024-01-09 18:41:43 +0100

    Update Botiga.java

--- Botiga/src/main/java/com/mycompany/botiga/Botiga.java
+++ Botiga/src/main/java/com/mycompany/botiga/Botiga.java
@@ -11,6 +11,6 @@ package com.mycompany.botiga;
 public class Botiga {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");WEE
     }
 }
